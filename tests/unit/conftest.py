72d5df2b04344205f85cb28e7ef5f47bb0689adc
rxvt <17208811+rxvt@users.noreply.github.com>
2023-02-05 18:50:35 +1100

    Move existing unit test file to 'unit' dir and rename to _old

---
+++ tests/unit/conftest.py
@@ -0,0 +1,25 @@
+"""S3Fetch conftest."""
+import os
+from typing import Generator
+
+import boto3
+import pytest
+from moto import mock_s3
+
+
+@pytest.fixture(scope="package", autouse=True)
+def aws_credentials():
+    """Mock AWS Credentials for Moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"  # noqa: S105
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"  # noqa: S105
+    os.environ["AWS_SESSION_TOKEN"] = "testing"  # noqa: S105
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+
+@pytest.fixture()
+def s3_client(scope="package") -> Generator:  # noqa: ANN001
+    """Return a mock S3Client object."""
+    with mock_s3():
+        client = boto3.client("s3", region_name="us-east-1")
+        yield client

c34e1e2592be86c705d328fa202b3d11f59f7b08
rxvt <17208811+rxvt@users.noreply.github.com>
2023-02-10 17:19:08 +1100

    Add S3FetchQueue

---
+++ src/s3fetch/s3.py
@@ -0,0 +1,22 @@
+from queue import Queue
+from .exceptions import S3FetchQueueEmpty
+
+class S3FetchQueue:
+    def __init__(self):
+        self.queue = Queue()
+
+    def put(self, key: str) -> None:
+        self.queue.put_nowait(key)
+
+    # TODO: Remove this after refactor, should only use self.put()
+    def put_nowait(self, key: str) -> None:
+        self.queue.put_nowait(key)
+
+    def get(self, block: bool = False) -> str:
+        key = self.queue.get(block=block)
+        if key is None:
+            raise S3FetchQueueEmpty
+        return key
+
+    def close(self) -> None:
+        self.queue.put_nowait(None)

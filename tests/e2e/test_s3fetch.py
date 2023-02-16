7a92023c45f71717f767bde6bf6095d100706582
rxvt <17208811+rxvt@users.noreply.github.com>
2023-02-17 10:47:41 +1100

    Add fixture to provide e2e test bucket details

--- tests/e2e/test_s3fetch.py
+++ tests/e2e/test_s3fetch.py
@@ -5,10 +5,19 @@ import pytest
 from s3fetch.command import S3Fetch
 
 
-def test_downloading_single_small_file(tmp_path):
+@pytest.fixture()
+def bucket_config() -> dict:
     config = {
-        "s3_uri": "s3://s3fetch-testing-us-east-2/small_files/01_small_test_file",
+        "bucket": "s3://s3fetch-testing-us-east-2",
         "region": "us-east-2",
+    }
+    return config
+
+
+def test_downloading_single_small_file(bucket_config, tmp_path):
+    config = {
+        "s3_uri": f"{bucket_config['bucket']}/small_files/01_small_test_file",
+        "region": bucket_config["region"],
         "download_dir": tmp_path,
     }
     s3fetch = S3Fetch(**config)
@@ -19,10 +28,10 @@ def test_downloading_single_small_file(tmp_path):
     assert file_contents == "This is the first test file.\n"
 
 
-def test_only_single_small_file_is_downloaded(tmp_path):
+def test_only_single_small_file_is_downloaded(bucket_config, tmp_path):
     config = {
-        "s3_uri": "s3://s3fetch-testing-us-east-2/small_files/01_small_test_file",
-        "region": "us-east-2",
+        "s3_uri": f"{bucket_config['bucket']}/small_files/01_small_test_file",
+        "region": bucket_config["region"],
         "download_dir": tmp_path,
     }
     s3fetch = S3Fetch(**config)

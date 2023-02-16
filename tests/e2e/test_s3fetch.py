513e7f11524edd00c2bd10b78ea503630c727bc1
rxvt <17208811+rxvt@users.noreply.github.com>
2023-02-17 10:48:12 +1100

    Add test to download single file in subdir

--- tests/e2e/test_s3fetch.py
+++ tests/e2e/test_s3fetch.py
@@ -38,3 +38,19 @@ def test_only_single_small_file_is_downloaded(bucket_config, tmp_path):
     s3fetch.run()
     download_dir = Path(tmp_path)
     assert len([download_dir.iterdir()]) == 1
+
+
+def test_downloading_single_small_file_in_sub_directory(bucket_config, tmp_path):
+    config = {
+        "s3_uri": f"{bucket_config['bucket']}/small_files",
+        "region": bucket_config["region"],
+        "download_dir": tmp_path,
+    }
+    s3fetch = S3Fetch(**config)
+    s3fetch.run()
+    sub_dir = Path(tmp_path) / "small_files"
+    testfile = sub_dir / "01_small_test_file"
+    assert sub_dir.is_dir()
+    assert testfile.is_file()
+    file_contents = testfile.read_text()
+    assert file_contents == "This is the first test file.\n"

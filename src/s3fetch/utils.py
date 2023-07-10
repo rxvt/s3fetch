7366f092f9b2658a4614cf9dcdf1461599e0af1d
rxvt <17208811+rxvt@users.noreply.github.com>
2023-07-10 13:51:42 +1000

    Add function docstring

--- src/s3fetch/utils.py
+++ src/s3fetch/utils.py
@@ -5,6 +5,13 @@ import click
 
 
 def tprint(msg: str, lock: Lock, quiet: bool = False) -> None:
+    """Thread safe print function.
+
+    Args:
+        msg (str): Message to print.
+        lock (Lock): Lock object.
+        quiet (bool, optional): Quiet mode enabled. Defaults to False.
+    """
     lock.acquire(timeout=1)
     if not quiet:
         click.echo(msg)

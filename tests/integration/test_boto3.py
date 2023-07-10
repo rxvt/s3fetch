31c9aceff3c0e96361fd2214dc25902c419dd582
rxvt <17208811+rxvt@users.noreply.github.com>
2023-07-10 13:54:03 +1000

    Ignore getattr linter complaint in test function

--- tests/integration/test_boto3.py
+++ tests/integration/test_boto3.py
@@ -1,11 +1,11 @@
 def test_default_max_pool_connections_is_10():
     from botocore import endpoint
 
-    assert getattr(endpoint, "MAX_POOL_CONNECTIONS") == 10
+    assert getattr(endpoint, "MAX_POOL_CONNECTIONS") == 10  # noqa: B009
 
 
 def test_default_s3_concurrency_is_10():
     from boto3.s3.transfer import TransferConfig
 
     config = TransferConfig()
-    assert getattr(config, "max_concurrency") == 10
+    assert getattr(config, "max_concurrency") == 10  # noqa: B009

55a7f71a355e7efb621dbd39c5eea12b9e4bfaa1
rxvt <17208811+rxvt@users.noreply.github.com>
2023-07-10 13:52:36 +1000

    Comment out all E2E tests for now

--- tests/e2e/test_s3fetch.py
+++ tests/e2e/test_s3fetch.py
@@ -1,56 +1,55 @@
 """End 2 End Tests for S3Fetch."""
+# from pathlib import Path
 
-import pytest
+# import pytest
+# from s3fetch.command import S3Fetch
 
-from s3fetch.command import S3Fetch
-
-
-@pytest.fixture()
-def bucket_config() -> dict:
-    config = {
-        "bucket": "s3://s3fetch-testing-us-east-2",
-        "region": "us-east-2",
-    }
-    return config
-
-
-def test_downloading_single_small_file(bucket_config, tmp_path):
-    config = {
-        "s3_uri": f"{bucket_config['bucket']}/small_files/01_small_test_file",
-        "region": bucket_config["region"],
-        "download_dir": tmp_path,
-    }
-    s3fetch = S3Fetch(**config)
-    s3fetch.run()
-    testfile = Path(tmp_path) / "01_small_test_file"
-    assert testfile.is_file()
-    file_contents = testfile.read_text()
-    assert file_contents == "This is the first test file.\n"
+# @pytest.fixture()
+# def bucket_config() -> dict:
+#     config = {
+#         "bucket": "s3://s3fetch-testing-us-east-2",
+#         "region": "us-east-2",
+#     }
+#     return config
 
 
-def test_only_single_small_file_is_downloaded(bucket_config, tmp_path):
-    config = {
-        "s3_uri": f"{bucket_config['bucket']}/small_files/01_small_test_file",
-        "region": bucket_config["region"],
-        "download_dir": tmp_path,
-    }
-    s3fetch = S3Fetch(**config)
-    s3fetch.run()
-    download_dir = Path(tmp_path)
-    assert len([download_dir.iterdir()]) == 1
+# def test_downloading_single_small_file(bucket_config, tmp_path):
+#     config = {
+#         "s3_uri": f"{bucket_config['bucket']}/small_files/01_small_test_file",
+#         "region": bucket_config["region"],
+#         "download_dir": tmp_path,
+#     }
+#     s3fetch = S3Fetch(**config)
+#     s3fetch.run()
+#     testfile = Path(tmp_path) / "01_small_test_file"
+#     assert testfile.is_file()
+#     file_contents = testfile.read_text()
+#     assert file_contents == "This is the first test file.\n"
 
 
-def test_downloading_single_small_file_in_sub_directory(bucket_config, tmp_path):
-    config = {
-        "s3_uri": f"{bucket_config['bucket']}/small_files",
-        "region": bucket_config["region"],
-        "download_dir": tmp_path,
-    }
-    s3fetch = S3Fetch(**config)
-    s3fetch.run()
-    sub_dir = Path(tmp_path) / "small_files"
-    testfile = sub_dir / "01_small_test_file"
-    assert sub_dir.is_dir()
-    assert testfile.is_file()
-    file_contents = testfile.read_text()
-    assert file_contents == "This is the first test file.\n"
+# def test_only_single_small_file_is_downloaded(bucket_config, tmp_path):
+#     config = {
+#         "s3_uri": f"{bucket_config['bucket']}/small_files/01_small_test_file",
+#         "region": bucket_config["region"],
+#         "download_dir": tmp_path,
+#     }
+#     s3fetch = S3Fetch(**config)
+#     s3fetch.run()
+#     download_dir = Path(tmp_path)
+#     assert len([download_dir.iterdir()]) == 1
+
+
+# def test_downloading_single_small_file_in_sub_directory(bucket_config, tmp_path):
+#     config = {
+#         "s3_uri": f"{bucket_config['bucket']}/small_files",
+#         "region": bucket_config["region"],
+#         "download_dir": tmp_path,
+#     }
+#     s3fetch = S3Fetch(**config)
+#     s3fetch.run()
+#     sub_dir = Path(tmp_path) / "small_files"
+#     testfile = sub_dir / "01_small_test_file"
+#     assert sub_dir.is_dir()
+#     assert testfile.is_file()
+#     file_contents = testfile.read_text()
+#     assert file_contents == "This is the first test file.\n"

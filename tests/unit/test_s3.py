5965224e969c978855df96bd958059e9d24ea583
rxvt <17208811+rxvt@users.noreply.github.com>
2023-07-10 13:39:00 +1000

    Import functions using namespace rather than directly

--- tests/unit/test_s3.py
+++ tests/unit/test_s3.py
@@ -1,48 +1,40 @@
 import pytest
-
-from s3fetch.exceptions import S3FetchQueueEmpty
-from s3fetch.s3 import (
-    S3FetchQueue,
-    check_if_key_is_directory,
-    filter_by_regex,
-    filter_object,
-    get_download_queue,
-)
+from s3fetch import s3
 
 
 def test_create_download_queue():
-    queue = get_download_queue()
-    assert isinstance(queue, S3FetchQueue)
+    queue = s3.get_download_queue()
+    assert isinstance(queue, s3.S3FetchQueue)
 
 
 def test_queue_raises_exception_when_sentinel_value_found():
-    queue = get_download_queue()
+    queue = s3.get_download_queue()
     queue.close()
-    with pytest.raises(S3FetchQueueEmpty):
+    with pytest.raises(s3.S3FetchQueueEmpty):
         queue.get()
 
 
 @pytest.mark.parametrize("delimiter", ["/", ":"])
 def test_exclude_directory_from_objects(delimiter):
     key = f"small_files{delimiter}"
-    result = check_if_key_is_directory(key=key, delimiter=delimiter)
+    result = s3.check_if_key_is_directory(key=key, delimiter=delimiter)
     assert result is True
 
 
 @pytest.mark.parametrize("delimiter", ["/", ":"])
 def test_not_excluding_non_directory_from_objects(delimiter):
     key = f"small_files{delimiter}my_photo"
-    result = check_if_key_is_directory(key=key, delimiter=delimiter)
+    result = s3.check_if_key_is_directory(key=key, delimiter=delimiter)
     assert result is False
 
 
 @pytest.mark.parametrize("key,", ["my_test_file", "my_dir/my_test_file"])
 def test_skip_keys_containing_only_letters(key):
-    result = filter_by_regex(key=key, regex=r"\d")
+    result = s3.filter_by_regex(key=key, regex=r"\d")
     assert result is False
 
 
 @pytest.mark.parametrize("key,", ["my_test_file", "my_dir/my_test_file"])
 def test_include_keys_starting_with_my_(key):
-    result = filter_by_regex(key=key, regex=r"^my_")
+    result = s3.filter_by_regex(key=key, regex=r"^my_")
     assert result is True

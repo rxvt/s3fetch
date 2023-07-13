1a0cd248d38fcb4aaca4c9531cf15f91ca434d42
rxvt <17208811+rxvt@users.noreply.github.com>
2023-07-13 16:22:03 +1000

    Add create_destination_directory() function
    Takes the download directory, object directory string and delimiter and
    builds the builds and creates the local absolute directory structure

---
+++ src/s3fetch/fs.py
@@ -0,0 +1,34 @@
+"""Filesystem utilities for S3Fetch."""
+from pathlib import Path
+from typing import Optional
+
+
+def create_destination_directory(
+    download_dir: Path,
+    object_dir: Optional[str],
+    delimiter: str,
+) -> Path:
+    """Create the local destination directory for the object.
+
+    Args:
+        download_dir (Path): The local download base dirctory.
+        object_dir (Union[str, Path]): The directory structure we will create for the
+            object under the base download directory.
+        delimiter (str): The delimiter used to split the object key into directories.
+
+    Returns:
+        Path: The absolute path to the local destination directory.
+    """
+    # Build the absolute directory path converting the object delimiter into a local
+    # directory delimiter.
+    if object_dir:
+        directories = object_dir.split(delimiter)
+        tmp_dir = Path()
+        for directory in directories:
+            tmp_dir = tmp_dir / Path(directory)
+        absolute_directory = download_dir / Path(tmp_dir)
+    else:
+        absolute_directory = download_dir
+
+    absolute_directory.mkdir(parents=True, exist_ok=True)
+    return absolute_directory
---
+++ tests/unit/test_fs.py
@@ -0,0 +1,23 @@
+import pytest
+from s3fetch import fs
+
+
+@pytest.mark.parametrize(
+    "object_dir, delimiter, expected_dir",
+    [
+        ("my/test/prefix", "/", "my/test/prefix"),
+        ("my/test/prefix/", "/", "my/test/prefix"),
+        ("my:test:prefix/", ":", "my/test/prefix"),
+        (None, "/", ""),
+    ],
+)
+def test_creating_destination_directory(tmp_path, object_dir, delimiter, expected_dir):
+    result = fs.create_destination_directory(
+        download_dir=tmp_path, delimiter=delimiter, object_dir=object_dir
+    )
+    if object_dir:
+        expected_dir = tmp_path / expected_dir
+    else:
+        expected_dir = tmp_path
+    assert expected_dir == result
+    assert expected_dir.is_dir()

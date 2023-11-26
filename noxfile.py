90ad75a4fd5fd583afd7cb13d478a743d2ce822d
rxvt <17208811+rxvt@users.noreply.github.com>
2023-11-26 19:27:58 +1100

    Add Python 3.12 testing support

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@
 import nox
 from nox_poetry import Session, session
 
-PYTHON_VERSIONS = ("3.9", "3.10", "3.11")
+PYTHON_VERSIONS = ("3.9", "3.10", "3.11", "3.12")
 MINIMUM_PYTHON_VERSION = ("3.9",)
 LINT_LOCATIONS = ("src", "tests", "./noxfile.py")
 

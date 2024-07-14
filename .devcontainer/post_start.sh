380ec0a4d2f05101d0d4b75474bffe6ad3c3a768
rxvt <17208811+rxvt@users.noreply.github.com>
2024-07-14 06:03:06 +0000

    Add pre-commit install step
    Runs when devcontainer starts

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -10,3 +10,7 @@ git config --global --add safe.directory "${CONTAINER_WORKSPACE_FOLDER}"
 # Remove SSH program configuration otherwise git inside the container will try to access the path for
 # the SSH program from the host machine. This causes problems specifically with the 1Password MacOS integration.
 git config --global --unset gpg.ssh.program
+
+# The following commands can't be run during the docker build process as the source directory is not mounted yet,
+# so we run them in the postCreateCommand instead.
+pre-commit install

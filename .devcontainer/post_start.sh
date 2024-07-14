986e650b14f1b2aab9d861a55045b80cc4d58240
rxvt <17208811+rxvt@users.noreply.github.com>
2024-07-14 05:54:45 +0000

    Remove git gpg.ssh.program config inside container
    Stops git inside the container trying to interact with a program that exists inside the host.

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -6,3 +6,7 @@ set -e
 # Fix git unsafe directory error as git dir is owned by `vscode` inside container
 # https://github.com/microsoft/vscode-remote-release/issues/6810#issuecomment-1310980232
 git config --global --add safe.directory "${CONTAINER_WORKSPACE_FOLDER}"
+
+# Remove SSH program configuration otherwise git inside the container will try to access the path for
+# the SSH program from the host machine. This causes problems specifically with the 1Password MacOS integration.
+git config --global --unset gpg.ssh.program

ea4522c7409f9f50d1105fbf3e000712b76fc18c
rxvt <17208811+rxvt@users.noreply.github.com>
2024-07-14 06:25:47 +0000

    Update initial pre-commit installation to run through hatch command

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -13,4 +13,5 @@ git config --global --unset gpg.ssh.program
 
 # The following commands can't be run during the docker build process as the source directory is not mounted yet,
 # so we run them in the postCreateCommand instead.
-pre-commit install --install-hooks
+# pre-commit install --install-hooks
+hatch run default:pre-commit install --install-hooks

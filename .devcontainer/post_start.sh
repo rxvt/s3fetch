6e94a59aad3dd171e8a01bf0bc9a2eb3e17f7974
rxvt <17208811+rxvt@users.noreply.github.com>
2024-07-14 05:52:58 +0000

    Fix git unsafe directory error

--- .devcontainer/post_start.sh
+++ .devcontainer/post_start.sh
@@ -2,3 +2,7 @@
 
 set -o pipefail
 set -e
+
+# Fix git unsafe directory error as git dir is owned by `vscode` inside container
+# https://github.com/microsoft/vscode-remote-release/issues/6810#issuecomment-1310980232
+git config --global --add safe.directory "${CONTAINER_WORKSPACE_FOLDER}"
